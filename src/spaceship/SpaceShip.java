ce8e0995c47564ae07cadf15844e743740f28954
juan9507 <juandavidnaranjo75@gmail.com>
2022-03-09 11:41:03 -0500

    fotogramas por segundo

--- src/spaceship/SpaceShip.java
+++ src/spaceship/SpaceShip.java
@@ -6,6 +6,7 @@
 package spaceship;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.SystemColor;
@@ -23,11 +24,15 @@ public class SpaceShip extends JFrame implements Runnable{ // extends JFrame par
     
     //Variables estaticas para la dimension de la ventana
     public static final int width_window = 800, height_window = 600;
-    private Canvas canvas; // Objeto para permiteir dibujar
+    private Canvas canvas; // Objeto para permitir dibujar
     private Thread thread;// Creacion de un hilo aparte del principal
     private boolean running = false; // controlar el estado del juego
     private BufferStrategy bs;
     private Graphics g;
+    private final int fps = 60; //Constante para fotograma por segundo
+    private double target_time = 1000000000/fps;  // Tiempo de el fotograma por nanosegundos
+    private double delta = 0; // Acumulador temporar para el tiempo que vaya pasando
+    private int average_fps = fps; // variable para saber a cuanto esta correndo el juego
     
     public SpaceShip(){ //Constructor 
         setTitle("Space Ship Game"); // Titulo de la ventana
@@ -67,8 +72,11 @@ public class SpaceShip extends JFrame implements Runnable{ // extends JFrame par
         
         g.clearRect(0, 0, getWidth(), getHeight());
         
-        g.drawRect(x, 0, 100, 100); // es para dibujar un rectangulo
+        //g.drawRect(x, 0, 100, 100); // es para dibujar un rectangulo
         
+        g.setColor(Color.blue);
+        
+        g.drawString(""+average_fps, 10, 10);
         //---------------------------------
         
         g.dispose();
@@ -77,11 +85,31 @@ public class SpaceShip extends JFrame implements Runnable{ // extends JFrame par
 
     @Override
     public void run() {
+        
+        long now = 0;
+        long lastTime = System.nanoTime(); // Nos da el tiempo en nanosegundos
+        int frames = 0; // variables para mostrar fotogramas por segundo
+        long time = 0;
+        
         // Ciclo principal que se encargara de actualizar la posicion de todos
         // los objetos del juego y dibujar una y potra vez
         while(running){
-            update();
-            draw();
+            now = System.nanoTime();
+            delta +=(now - lastTime)/ target_time; //obtener el tiempo que allá pasado
+            time += (now - lastTime);
+            lastTime = now;
+            
+            if(delta >= 1){
+                update();
+                draw();
+                delta--;
+                frames ++;
+            }
+            if(time >= 1000000000){ 
+                average_fps = frames;
+                frames = 0;
+                time = 0;
+            }
         }
         stop();
     }
